746a2aa1f0ff76727d87a5a56c99f373e4c8c2bb
Isaac Mawuena Agbogah <isaacagbogah1@gmail.com>
2023-09-29 14:44:24 +0000

    Updated App

--- app/app.py
+++ app/app.py
@@ -1,18 +1,40 @@
 import gradio as gr
-from tensorflow import keras
+import joblib  # Assuming your RFC model is saved as a joblib file
+import numpy as np
+from sklearn.preprocessing import StandardScaler  # Import any necessary preprocessing modules
 
-# Load the TensorFlow model
-model = keras.models.load_model('my_model.h5')
+# Load your RFC model
+model_path = "path_to_your_rfc_model.joblib"
+rfc_model = joblib.load(model_path)
 
-# Define a function that takes the input data and returns the output of the model
+# Load your preprocessing steps, if any
+# For example, if you used StandardScaler during training, load it here
+scaler = joblib.load("path_to_your_scaler.joblib")
+
+# Define a function for making predictions using the RFC model
 def predict(input_data):
-    
-    # Apply the model to the input data and return the output
-    model_output = model.predict(input_data)
-    return model_output
+    # Preprocess your input data
+    # For example, convert input_data to a NumPy array
+    input_data = np.array(input_data).reshape(1, -1)
 
-# Create a Gradio interface that takes the input data and displays the output of the model
-interface = gr.Interface(fn=predict, inputs="text", outputs="text")
+    # Apply any necessary preprocessing steps
+    if scaler is not None:
+        input_data = scaler.transform(input_data)
 
-# Launch the Gradio interface
-interface.launch()
+    # Make predictions using the RFC model
+    prediction = rfc_model.predict(input_data)
+
+    # Return the prediction result
+    return prediction[0]
+
+# Create the Gradio interface
+input_textbox = gr.inputs.Textbox(label="Input Data")
+output_textbox = gr.outputs.Textbox(label="Prediction")
+
+gr.Interface(
+    fn=predict,
+    inputs=input_textbox,
+    outputs=output_textbox,
+    layout="vertical",
+    title="RFC Model Predictor",
+).launch()
