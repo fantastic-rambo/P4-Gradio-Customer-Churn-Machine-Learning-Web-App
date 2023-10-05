de4751f549eacdb2da70df5b2caca33ca62e69f5
Isaac Mawuena Agbogah <isaacagbogah1@gmail.com>
2023-10-05 09:51:11 +0000

    Updated App

--- app/app.py
+++ app/app.py
@@ -1,40 +1,70 @@
 import gradio as gr
-import joblib  # Assuming your RFC model is saved as a joblib file
+import joblib
+import pandas as pd
 import numpy as np
-from sklearn.preprocessing import StandardScaler  # Import any necessary preprocessing modules
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 
-# Load your RFC model
-model_path = "path_to_your_rfc_model.joblib"
-rfc_model = joblib.load(model_path)
+# Load your pre-trained machine learning model
+model = joblib.load(r"C:\Users\IKE\OneDrive - Azubi Africa\Project1\P4-Gradio-Customer-Churn-Machine-Learning-Web-App\model\RFC_model.joblib")
 
-# Load your preprocessing steps, if any
-# For example, if you used StandardScaler during training, load it here
-scaler = joblib.load("path_to_your_scaler.joblib")
+# Load any preprocessing objects used during model training
+scaler = joblib.load(r"C:\Users\IKE\OneDrive - Azubi Africa\Project1\P4-Gradio-Customer-Churn-Machine-Learning-Web-App\model\preprocessor.joblib")
 
-# Define a function for making predictions using the RFC model
-def predict(input_data):
-    # Preprocess your input data
-    # For example, convert input_data to a NumPy array
-    input_data = np.array(input_data).reshape(1, -1)
 
-    # Apply any necessary preprocessing steps
-    if scaler is not None:
-        input_data = scaler.transform(input_data)
+# Define a function for making predictions
+def predict_churn(
+    #customerID,
+    gender, SeniorCitizen, Partner, Dependents, tenure, MonthlyCharges
+):
+    
+    
+    # Prepare the input data in a DataFrame
+    input_data = pd.DataFrame(
+        {
+            #"customerID": [customerID],
+            "gender": [gender],
+            "senior_citizen": [SeniorCitizen],
+            "partner": [Partner],
+            "dependents": [Dependents],
+            "tenure": [tenure],
+            "MonthlyCharges": [MonthlyCharges],
+        }
+    )
 
-    # Make predictions using the RFC model
-    prediction = rfc_model.predict(input_data)
+    # Apply the same preprocessing steps used during training
+    input_data = scaler.transform(input_data)
 
-    # Return the prediction result
-    return prediction[0]
+
+    # Make the churn prediction
+    prediction = model.predict(input_data)
+
+    # Return a user-friendly result
+    if prediction[0] == 1:
+        result = "Churn"
+    else:
+        result = "No Churn"
+
+    return result
+
 
 # Create the Gradio interface
-input_textbox = gr.inputs.Textbox(label="Input Data")
-output_textbox = gr.outputs.Textbox(label="Prediction")
+interface = gr.Interface(
+    fn=predict_churn,
+    inputs=[
+        #gr.components.Textbox(label="Customer ID"),
+        gr.components.Dropdown(label="Gender", choices=["Male", "Female", "Other"]),
+        gr.components.Number(label="Senior Citizen (0 for No, 1 for Yes)"),
+        gr.components.Dropdown(label="Partner", choices=["Yes", "No"]),
+        gr.components.Dropdown(label="Dependents", choices=["Yes", "No"]),
+        gr.components.Number(label="Tenure (months)"),
+        gr.components.Number(label="Monthly Charges"),
+    ],
+    outputs=gr.components.Textbox(label="Prediction"),
+    title="Telco Customer Churn Prediction App",
+    description="Please Enter Customer Information to Predict Churn.",
+)
 
-gr.Interface(
-    fn=predict,
-    inputs=input_textbox,
-    outputs=output_textbox,
-    layout="vertical",
-    title="RFC Model Predictor",
-).launch()
+# Launch the Gradio interface
+interface.launch(debug=True)
